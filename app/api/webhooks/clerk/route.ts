ee605b2dd574b4236474f76a4aba8e6098f9824f
thecodingrunner <finn.moffett@gmail.com>
2024-07-07 13:12:35 +0100

    Remove redirects

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -11,6 +11,8 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
+  console.log(WEBHOOK_SECRET)
+
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -69,6 +71,8 @@ export async function POST(req: Request) {
       lastName: last_name as string,
       photo: image_url,
     };
+
+    console.log(user)
 
     const newUser = await createUser(user);
 
