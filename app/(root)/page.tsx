235a9236803d6a6b1e4ed2d4d317dc3ef42957b4
thecodingrunner <finn.moffett@gmail.com>
2024-07-07 12:03:47 +0100

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { SignedIn } from '@clerk/clerk-react'
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Home
